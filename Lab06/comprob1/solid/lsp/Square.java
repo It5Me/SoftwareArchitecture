36ed2cb3466e88a6ef25dab37530bd1a7cf5985c
It5Me <56817602+It5Me@users.noreply.github.com>
2021-10-01 22:35:37 +0700

    problem1

---
+++ Lab06/comprob1/solid/lsp/Square.java
@@ -0,0 +1,18 @@
+package comprob1.solid.lsp;
+
+public class Square {
+    private int side;
+    Square(int side) {
+        this.setSide(side);
+    }
+    public void setSide(int side) {
+        this.side = side;
+    }
+
+    public int getSide() {
+        return side;
+    }
+    public int getArea() {
+      return side*side;
+    }
+}
